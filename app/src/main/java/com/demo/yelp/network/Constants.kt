c76bacf8552538302b4bea9c7085d2d95f2c5a6c
bhushan2k <bhushan2k@users.noreply.github.com>
2022-11-07 17:27:25 +0530

    Update Constants.kt
    added placeholder for api key

--- app/src/main/java/com/demo/yelp/network/Constants.kt
+++ app/src/main/java/com/demo/yelp/network/Constants.kt
@@ -2,5 +2,5 @@ package com.demo.yelp.network
 
 val BASE_URL = "https://api.yelp.com/"
 var API_KEY =
-    "Bearer XPFgzKwZGK1yqRxHi0d5xsARFOLpXIvccQj5jekqTnysweGyoIfVUHcH2tPfGq5Oc9kwKHPkcOjk2d1Xobn7aTjOFeop8x41IUfVvg2Y27KiINjYPADcE7Qza0RkX3Yx"
+    "Bearer <API_KEY>"
 var DEMO_CITY = "New York City"
